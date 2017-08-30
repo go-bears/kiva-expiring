4feaf88d5f249d9766678bf4d1127cb5e7c0a776
m.fab <go-bears@users.noreply.github.com>
2017-08-29 21:41:37 -0700

    refactor show_total_amount_needed for unit testing

--- kiva_expiring_loans.py
+++ kiva_expiring_loans.py
@@ -11,7 +11,7 @@ code does the right calculation and returns the right values.
 __author__ = "melissa fabros"
 __license__ = "BSD"
 __version__ = "0.1"
-__status__ = "Prototype" 
+__status__ = "Prototype"
 
 
 import requests
@@ -24,7 +24,7 @@ from dateutil import parser
 GRAPHQL_ENDPOINT = 'https://api.kivaws.org/graphql?query='
 LEND_TAB_URL_BASE = 'https://www.kiva.org/lend/'
 NOW = datetime.now(pytz.utc)
-print (NOW)
+
 
 query_loans_expiring_24hrs = """
 { loans (
@@ -53,22 +53,20 @@ query_loans_expiring_24hrs = """
 """
 
 
-
 def call_kivaapi(query):
     """takes in graphql style query as a string
     return JSON response of loan data as list of dictionaries
     """
-    
+
     try:
         response = requests.get(GRAPHQL_ENDPOINT + query, timeout=10)
         print("OK: status {}.".format(response.status_code))
-    
+
         data = response.json()
         loans_data = data['data']['loans']['values']
 
-        print (loans_data)
         return loans_data
-    
+
     except requests.exceptions.HTTPError as err:
         print (err)
 
@@ -78,17 +76,19 @@ def preprocess_json(json_response_list):
     return list with converted values and new key-values for links to loans
     and amount left to fundraise
     """
-    
+
     if not json_response_list:
         raise ValueError("list is empty")
-    
+
     for loan in json_response_list:
         loan['loanAmount'] = float(loan['loanAmount'])
-        loan['loanFundraisingInfo']['fundedAmount'] = float(loan['loanFundraisingInfo']['fundedAmount'])
+        loan['loanFundraisingInfo']['fundedAmount'] = float(
+            loan['loanFundraisingInfo']['fundedAmount'])
         loan['link'] = LEND_TAB_URL_BASE + str(loan['id'])
-        loan['amtLeftToFundraise']  = loan['loanAmount'] - loan['loanFundraisingInfo']['fundedAmount']
-        loan['plannedExpirationDate'] = parser.parse(loan['plannedExpirationDate'])
-
+        loan['amtLeftToFundraise'] = loan['loanAmount'] - \
+            loan['loanFundraisingInfo']['fundedAmount']
+        loan['plannedExpirationDate'] = parser.parse(
+            loan['plannedExpirationDate'])
 
     return json_response_list
 
@@ -97,15 +97,14 @@ def filter_loans_24_hrs(processed_json_list):
     """takes in list of cleaned data and returns subset list
     of loans that expire within 24 hours of current date/time
     """
-    
+
     delta = timedelta(hours=24)
-    
-    
+
     if not processed_json_list:
         raise ValueError("list is empty")
-        
+
     sorted_loans = []
-    
+
     for loan in processed_json_list:
         planned_expiration = loan['plannedExpirationDate']
 
@@ -117,67 +116,74 @@ def filter_loans_24_hrs(processed_json_list):
     return sorted_loans
 
 
-def show_total_fundraising_needed(subset_24_hrs_list):
+def calculate_total_fundraising_needed(subset_24_hrs_list):
     """takes in list of filtered loan data and displays
     float of total for all loans needing funding that expire in 
     24 hours
     """
-    
+
     if not subset_24_hrs_list:
         raise ValueError("list is empty")
-    
+
     total = 0
-    
+
     if subset_24_hrs_list:
         for loan in subset_24_hrs_list:
             if loan['amtLeftToFundraise'] > 0:
                 total += loan['amtLeftToFundraise']
             else:
                 print("This loan id {} is reporting negative value {}:".format(loan['id'],
-                															   loan['amtLeftToFundraise']))
+                                                                               loan['amtLeftToFundraise']))
                 continue
-        
-        print("Total amount needed to fund loans expiring within 24 hours: {0:.2f} \n".format(total))
+
+    return total
+
+def show_total_fundraising_needed(total_sum):
+    msg = "Total amount needed to fund loans expiring within 24 hours: {0:.2f} \n".format(total_sum)
+    # print (msg)
+    
+    return msg
 
 
 def show_expiring_loans(subset_24_hrs_list):
     """takes in list of filtered loan data and displays links and amount remaining
         left to fund        
     """
-    
+
     if not subset_24_hrs_list:
         raise ValueError("list is empty")
-        
+
     if len(subset_24_hrs_list) < 2:
         print('This loan needs your help! : \n')
     else:
         print('These loans need your help! : \n')
-    
+
     for loan in subset_24_hrs_list:
         if loan['amtLeftToFundraise'] > 0:
-            print (loan['link'], "amount left to raise: {0:.2f}".format(loan['amtLeftToFundraise'] ))
-            
+            print (loan['link'], "amount left to raise: {0:.2f}".format(
+                loan['amtLeftToFundraise']))
+
         else:
             print("This loan id {} is reporting negative value {}:".format(loan['id'],
-            															   loan['amtLeftToFundraise']))
+                                                                           loan['amtLeftToFundraise']))
             continue
 
 
-
 def main():
-	"""runs all functions for execution and testing
-	"""
-
-	loans = call_kivaapi(query_loans_expiring_24hrs)
-	# print(len(loans))
-	test_loans = loans
-	# print(len(test_loans))
-	test1 = preprocess_json(test_loans)
-	# print (len(test1))
-	test2 = filter_loans_24_hrs(test1)
-	# print(len(test2))
-	show_total_fundraising_needed(test2)
-	show_expiring_loans(test2)
+    """runs all functions for execution and testing
+    """
+ 
+    loans = call_kivaapi(query_loans_expiring_24hrs)
+    print(len(loans))
+    test_loans = loans
+    # print(len(test_loans))
+    test1 = preprocess_json(test_loans)
+    # print (len(test1))
+    test2 = filter_loans_24_hrs(test1)
+    # print(len(test2))
+    total = calculate_total_fundraising_needed(test2)
+    show_total_fundraising_needed(total)
+    show_expiring_loans(test2)
 
 if __name__ == '__main__':
-  	main()  
+    main()
